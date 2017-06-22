d3a0c79b012bab6abf95055fa0f9742c20b5cfa3
Eduardo Sanz García <esanzgar@gmail.com>
2017-06-22 13:17:33 +0100

    Added WebDAV methods.

---
+++ src/backends/browser_jsonp.js
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var _nextRequestId = 0;
+exports.JSONP_HOME = '__ng_jsonp__';
+var _jsonpConnections = null;
+function _getJsonpConnections() {
+    var w = typeof window == 'object' ? window : {};
+    if (_jsonpConnections === null) {
+        _jsonpConnections = w[exports.JSONP_HOME] = {};
+    }
+    return _jsonpConnections;
+}
+// Make sure not to evaluate this in a non-browser environment!
+var BrowserJsonp = (function () {
+    function BrowserJsonp() {
+    }
+    // Construct a <script> element with the specified URL
+    BrowserJsonp.prototype.build = function (url) {
+        var node = document.createElement('script');
+        node.src = url;
+        return node;
+    };
+    BrowserJsonp.prototype.nextRequestID = function () { return "__req" + _nextRequestId++; };
+    BrowserJsonp.prototype.requestCallback = function (id) { return exports.JSONP_HOME + "." + id + ".finished"; };
+    BrowserJsonp.prototype.exposeConnection = function (id, connection) {
+        var connections = _getJsonpConnections();
+        connections[id] = connection;
+    };
+    BrowserJsonp.prototype.removeConnection = function (id) {
+        var connections = _getJsonpConnections();
+        connections[id] = null;
+    };
+    // Attach the <script> element to the DOM
+    BrowserJsonp.prototype.send = function (node) { document.body.appendChild((node)); };
+    // Remove <script> element from the DOM
+    BrowserJsonp.prototype.cleanup = function (node) {
+        if (node.parentNode) {
+            node.parentNode.removeChild((node));
+        }
+    };
+    return BrowserJsonp;
+}());
+BrowserJsonp = __decorate([
+    core_1.Injectable()
+], BrowserJsonp);
+exports.BrowserJsonp = BrowserJsonp;
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,22 @@
-import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
 
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
-
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    SampleDirective,
-    SamplePipe
-  ],
-  exports: [
-    SampleDirective,
-    SamplePipe
-  ]
-})
-export class SampleModule {
-  static forRoot(): ModuleWithProviders {
-    return {
-      ngModule: SampleModule,
-      providers: [SampleService]
-    };
-  }
-}
+export {BrowserXhr} from './backends/browser_xhr';
+export {JSONPBackend, JSONPConnection} from './backends/jsonp_backend';
+export {CookieXSRFStrategy, XHRBackend, XHRConnection} from './backends/xhr_backend';
+export {BaseRequestOptions, RequestOptions} from './base_request_options';
+export {BaseResponseOptions, ResponseOptions} from './base_response_options';
+export {ReadyState, RequestMethod, ResponseContentType, ResponseType} from './enums';
+export {Headers} from './headers';
+export {Http, Jsonp} from './http';
+export {HttpModule, JsonpModule} from './http_module';
+export {Connection, ConnectionBackend, RequestOptionsArgs, ResponseOptionsArgs, XSRFStrategy} from './interfaces';
+export {Request} from './static_request';
+export {Response} from './static_response';
+export {QueryEncoder, URLSearchParams} from './url_search_params';
+export {VERSION} from './version';
